5bc8e1647419d33c7a6d4cae535864499e7fa2a9
puncika <ooksetam@gmail.com>
2025-09-19 09:23:40 +0200

    Added tutorial for cloudflare generated url for testing purposes on other devices

--- src/env.js
+++ src/env.js
@@ -6,6 +6,8 @@ const host =
 const url =
   process.argv.includes("--dev") == 1
     ? "http://localhost:61613/"
+    // for testing on other devices (like androidTV or so...), for example you can use command "npx cloudflared tunnel --url http://localhost:61613" and put the generated url here down
+    // Then start the addon without --dev: "node src/server.js"
     : "https://20317bf4c6c6-webshare-stremio-addon.baby-beamup.club/";
 
 module.exports = { host, url };
